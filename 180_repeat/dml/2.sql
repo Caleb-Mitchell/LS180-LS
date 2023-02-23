df189a581de71a1cbbd13cfc5fc800bafafddadb
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-02-23 13:45:30 -0600

    Finish second pass through dml exercises

---
+++ 180_repeat/dml/2.sql
@@ -0,0 +1,19 @@
+-- INSERT INTO devices (
+--   name
+-- ) VALUES ( 'Accelerometer' ),
+--          ( 'Gyroscope');
+
+INSERT INTO parts (
+  part_number, device_id
+
+) VALUES ( 100, 1 ),
+         ( 150, 1 ),
+         ( 200, 1 ),
+         ( 250, 2 ),
+         ( 350, 2 ),
+         ( 450, 2 ),
+         ( 550, 2 ),
+         ( 650, 2 ),
+         ( 750, NULL ),
+         ( 850, NULL ),
+         ( 950, NULL );
---
+++ 180_repeat/dml/9.sql
@@ -0,0 +1,3 @@
+UPDATE parts
+  SET device_id = 1
+  WHERE id = 18 OR id = 19;
